e70df74a51fdf48680ef4047226ef7134f63f778
Sydney Ang Peliño <sydneyang1234@gmail.com>
2025-07-30 10:59:49 +0800

    refactor: update main app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { VideosModule } from './videos/videos.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DatabaseModule, VideosModule],
 })
 export class AppModule {}
