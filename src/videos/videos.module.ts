d31511b4b4a239396b5c60c68dfb8c4da7d23bad
Sydney Ang Peliño <sydneyang1234@gmail.com>
2025-07-31 10:47:53 +0800

    refactor: export service and update imports

--- src/videos/videos.module.ts
+++ src/videos/videos.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Video } from './entities/video.entity';
 import { VideosService } from './videos.service';
 import { VideosController } from './videos.controller';
-import { Video } from './entities/video.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Video])],
   controllers: [VideosController],
   providers: [VideosService],
+  exports: [VideosService],
 })
 export class VideosModule {}
